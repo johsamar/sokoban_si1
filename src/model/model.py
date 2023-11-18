89d181baeab0bfca249bb6bbf26b9561a9049868
mcgp <51185772+mcgp@users.noreply.github.com>
2023-11-18 11:18:35 -0500

    Carga y representacion del mapa con los agentes

--- src/model/model.py
+++ src/model/model.py
@@ -7,37 +7,66 @@ from mesa.time import RandomActivation
 from mesa.space import MultiGrid
 
 class SokobanModel(Model):
-    def __init__(self,width,height,algorithm=None, heuristic=None):
+    def load_map(self, filename):
+        with open(filename, 'r') as file:
+            map_string = file.read()
+        map_list = [line.split(', ') for line in map_string.splitlines()]
+        print(map_list) 
+        self.width = len(map_list[0]) 
+        self.height = len(map_list)
+        return map_list
 
-        self.grid = MultiGrid(width, height, True)
+    def __init__(self, algorithm=None, heuristic=None, filename=None):
         self.schedule = RandomActivation(self)
         self.algorithm = algorithm
         self.heuristic = heuristic
+        self.filename = filename
 
-        """class SokobanModel(Model):
-    def __init__(self, N, width, height, search_type="uninformed", heuristic="heuristic1"):
-        # ..."""
-        robot = RobotAgent(1, self)
-        wall = WallAgent(2, self)
-        box = BoxAgent(3, self)
-        finish = FinishAgent(4, self)
+        # Carga el mapa
+        self.map = self.load_map(filename)
 
-        x_robot, y_robot = self.random.randrange(self.grid.width), self.random.randrange(self.grid.height)
-        x_wall, y_wall = self.random.randrange(self.grid.width), self.random.randrange(self.grid.height)
-        x_box, y_box = self.random.randrange(self.grid.width), self.random.randrange(self.grid.height)
-        x_finish, y_finish = self.random.randrange(self.grid.width), self.random.randrange(self.grid.height)
+        # Inicializa la cuadrícula con la altura y anchura del mapa
+        self.grid = MultiGrid(self.width, self.height, True)
 
-        self.grid.place_agent(robot, (x_robot, y_robot))
-        self.grid.place_agent(wall, (x_wall, y_wall))
-        self.grid.place_agent(box, (x_box, y_box))
-        self.grid.place_agent(finish, (x_finish, y_finish))
+        agent_id = 0
+        for y in range(self.height):
+            for x in range(self.width):
+                grid_content = self.map[y][x]
+                if 'C-a' in grid_content:
+                    robot = RobotAgent(agent_id, self)
+                    self.grid.place_agent(robot, (x, y))
+                    self.schedule.add(robot)
+                    agent_id += 1
+                elif 'C-b' in grid_content:
+                    box = BoxAgent(agent_id, self)
+                    self.grid.place_agent(box, (x, y))
+                    self.schedule.add(box)
+                    agent_id += 1
+                elif 'R' in grid_content:
+                    wall = WallAgent(agent_id, self)
+                    self.grid.place_agent(wall, (x, y))
+                    self.schedule.add(wall)
+                    agent_id += 1
+                elif 'M' in grid_content:
+                    finish = FinishAgent(agent_id, self)
+                    self.grid.place_agent(finish, (x, y))
+                    self.schedule.add(finish)
+                    agent_id += 1
+        # Resto del código del modelo sin cambios
 
-        self.schedule.add(robot)
-        self.schedule.add(wall)
-        self.schedule.add(box)
-        self.schedule.add(finish)
-
-
+    # Agregar propiedades 
+    
+     
+    def print_grid(self):
+        for y in range(self.grid.height):
+            for x in range(self.grid.width):
+                cell = self.grid.get_cell_list_contents([(x, y)])
+                if len(cell) > 0:
+                    print(cell[0].__class__.__name__[0], end='')
+                else:
+                    print(' ', end='')
+            print()
 
     def step(self) -> None:
         self.schedule.step()
+        self.print_grid()
--- src/server.py
+++ src/server.py
@@ -28,13 +28,11 @@ def agent_portrayal(agent):
 
 
 def seleccionar_algoritmo():
-
     global variable, variable_heuristica
     seleccion = variable.get()
-    #print("Algoritmo seleccionado:", seleccion)
-    heuristica= variable_heuristica.get() 
+    heuristica = variable_heuristica.get() 
     return seleccion, heuristica
-    
+
 num_row_width = 5
 num_row_height = 5
 
@@ -46,7 +44,6 @@ root.title("Selección de Algoritmo")
 opciones = ["Algoritmo No informado", "Algoritmo Informado"]
 variable = tk.StringVar(root)
 variable.set(opciones[0]) 
-
 
 # Lista de opciones de heurísticas
 opciones_heuristica = ["Manhattan ", "Euclidiana"]
@@ -64,32 +61,39 @@ menu_heuristica.pack()
 # Botón para confirmar selección
 boton = tk.Button(root, text="Seleccionar", command=seleccionar_algoritmo)
 boton.pack()
-
-
-    
+  
 root.mainloop()
 
+# Obtener la selección del algoritmo y heurística
+algoritmo_seleccionado, heuristica_seleccionada = seleccionar_algoritmo()
 
 # Crear la cuadrícula para la visualización
-grid = CanvasGrid(agent_portrayal, num_row_width, num_row_height, 500, 500)
+#grid = CanvasGrid(agent_portrayal, num_row_width, num_row_height, 500, 500)
+#server = ModularServer(SokobanModel, [ViewElement()], "Titulo")
+
+#width = server.model.width
+#height = server.model.height
+filename = "maps/map1.txt"
+
+model = SokobanModel(filename="maps/map1.txt")
+grid = CanvasGrid(agent_portrayal, model.grid.width, model.grid.height, 500, 500)
+
+#grid = CanvasGrid(agent_portrayal, width, height, 500, 500) 
 
 # Crear el servidor con el modelo Sokoban y la cuadrícula
-#server = ModularServer(SokobanModel, [grid], "Sokoban Model", {"width": num_row_width, "height": num_row_height, "algorithm": seleccionar_algoritmo()})
-#server.port = 8521
-
-algoritmo_seleccionado, heuristica_seleccionada = seleccionar_algoritmo()
-algoritmo_element = ViewElement().algoritmo_render
-heuristic_element = ViewElement().heuristic_render
-
-
-server = ModularServer(SokobanModel, [grid,algoritmo_element,heuristic_element], "Sokoban Model", {"width": num_row_width, "height": num_row_height, "algorithm": algoritmo_seleccionado, "heuristic": heuristica_seleccionada})
+server = ModularServer(
+    SokobanModel,
+    [grid, ViewElement()],
+    "Sokoban Model",
+    {
+       
+        "algorithm": algoritmo_seleccionado,
+        "heuristic": heuristica_seleccionada,
+        "filename": filename  # Nombre del archivo del mapa
+    }
+)
 server.port = 8521
 print("Algoritmo en el modelo:", server.model.algorithm)
 print("Heuristica en el modelo:", server.model.heuristic)
 
 server.launch()
-#root.update()
-#root.mainloop()
-#print("Algoritmo en el modelo:", server.model.algorithm)
-#server.launch()
-
